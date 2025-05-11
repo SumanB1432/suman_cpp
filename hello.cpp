c67c7479634221b0e7c7a027ef5520d8b05dd927
Suman Bera <suman80bera@gmail.com>
2025-05-11 17:59:52 +0530

    hello world

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout << "Hello World";
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout << "My Name is" <<endl;
+//     cout <<"Suman Bera" <<endl;
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout <<"Suman" << "\n" << "Bera" << "\n";
+    return 0;
+}
